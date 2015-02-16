a9c544b646196cb8c2754deb6235f7ae8c731f98
daiqiu <daiqiu@appgame.com>
2015-02-16 16:51:39 +0800

    add dpendency - mime; update handler for static contents;

--- lib/src/dart_mvc_base.dart
+++ lib/src/dart_mvc_base.dart
@@ -5,40 +5,59 @@ library dart_mvc.base;
 
 import 'dart:io';
 import 'package:path/path.dart' as path;
+import 'package:mime/mime.dart' as mime;
 
 class MvcServer {
+
+  String contentsFolder = 'contents';
 
   void run() {
     HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080).then((server) {
       print("Serving at ${server.address}:${server.port}");
       server.listen((HttpRequest request) {
-        serve(request);
+        _serve(request);
       });
     });
   }
 
-  void serve(HttpRequest req) {
-    var reqPath = req.uri.path;
+  void _serve(HttpRequest req) {
+    var reqPath = req.uri.toString();
     print("Request: ${reqPath}  ${new DateTime.now()}");
-    var ext = path.extension(reqPath);
-    if (ext.length > 0) {
-      handleContent(req);
-    } else {
-      handleDynamic(req);
+
+    // skip favicon
+    if (reqPath != '/favicon.ico') {
+      var ext = path.extension(reqPath);
+      if (ext.length > 0) {
+        _handleContent(req, reqPath);
+      } else {
+        _handleDynamic(req);
+      }
     }
   }
 
-  void handleContent(HttpRequest req) {
-    req.response
-      ..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
-      ..write('Hello, Content')
-      ..close();
+  void _handleContent(HttpRequest req, String filePath) {
+    final CONTENTS_PATH = Platform.script.resolve(contentsFolder).toFilePath();
+    var file = new File(CONTENTS_PATH + filePath);
+    if (file.existsSync()) {
+      var mimeType = mime.lookupMimeType(file.path);
+      req.response.headers.set('Content-Type', mimeType);
+      file.openRead().pipe(req.response).catchError((e) {});
+    } else {
+      _notFound(req);
+    }
   }
 
-  void handleDynamic(HttpRequest req) {
+  void _handleDynamic(HttpRequest req) {
     req.response
       ..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
       ..write('Hello, Dynamic')
+      ..close();
+  }
+
+  void _notFound(HttpRequest req) {
+    req.response
+      ..statusCode = HttpStatus.NOT_FOUND
+      ..write('Not found!')
       ..close();
   }
 }
